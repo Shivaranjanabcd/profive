d39a20981641ca29315365d36cbfc12ee219c914
shivu <shivaranjana2z@gmail.com>
2024-04-22 14:40:24 +0530

    1stage

--- pro/app/views.py
+++ pro/app/views.py
@@ -1,38 +1,63 @@
 from django.shortcuts import render
 from .forms import MyForms
 from django.contrib import messages
+from django.core.mail import send_mail
+
+
 # Create your views here.
 def home(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = MyForms(request.POST)
         if form.is_valid():
-            messages.success(request,("Your Request Submited!"))
+            name = request.POST["name"]
+            phone = request.POST["phone"]
+            mail = request.POST["mail"]
+            serv = request.POST["serv"]
+            mess = request.POST["mess"]
+            subject = serv
+            message = f"Name : {name}\nPhone Number : {phone}\nMail : {mail}\nService : {serv}\nMessage : {mess}"
+            from_email = mail
+            recipient_list = [
+                "shivaranjana2z@gmail.com",
+                "rakipa1015@gmail.com",
+                "Varuna@profiveinfotech.com",
+            ]
+            send_mail(subject, message, from_email, recipient_list)
+
+            messages.success(request, ("Your Request Submited!"))
         else:
-            messages.success(request,("Invalid Captcha!"))
+            messages.success(request, ("Invalid Captcha!"))
     form = MyForms()
-    return render(request,"index.html",{'form':form})
+    return render(request, "index.html", {"form": form})
 
 
 def service(request):
-    return render(request,"service-details.html",{})
+    return render(request, "service-details.html", {})
+
 
 def it_infra_service(request):
-    return render(request,"it_infra_service.html",{})
+    return render(request, "it_infra_service.html", {})
+
 
 def cloud_service(request):
-    return render(request,"cloud_service.html",{})
+    return render(request, "cloud_service.html", {})
+
 
 def contact(request):
-    return render(request, "contact.html",{})
+    return render(request, "contact.html", {})
+
 
 def blog(request):
-    return render(request, "blog.html",{})
+    return render(request, "blog.html", {})
+
 
 def project(request):
-    return render(request, "project.html",{})
+    return render(request, "project.html", {})
+
 
 def about(request):
-    return render(request, "about.html",{})
+    return render(request, "about.html", {})
+
 
 def manged_it(request):
-    return render(request,'manged_it.html',{})
+    return render(request, "manged_it.html", {})
--- pro/pro/settings.py
+++ pro/pro/settings.py
@@ -132,3 +132,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# settings.py
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'raki631999@gmail.com'  # Your Gmail email address
+EMAIL_HOST_PASSWORD = 'jwvzqznqflrraazo'     # Your Gmail app password (if 2-factor authentication is enabled)
